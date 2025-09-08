390c0e7a89c472e98d1f7ad4c1d240b4d0bddf21
Brent Fitzgerald <brent@mono-koto.com>
2025-09-07 17:43:28 -0700

    fix vite base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pyusd-simple-splitter/',
+  base: '/pyusd-arbitrum-splitter/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
